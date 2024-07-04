8c524d3488318ce96b11898a8fe4f4e321f39ee1
Anton Radtsevich <ultimatovski@gmail.com>
2024-07-04 17:27:39 +0300

    removed unused libraries

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 //import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './App';
-import store from './store/configureStore'; // Ensure this path is correct
 
 const container = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(container);
 
 root.render(
-    <Provider store={store}>
       <App />
-    </Provider>
 );
--- src/types/types.ts
+++ src/types/types.ts
@@ -16,25 +16,5 @@ export interface CharacterResponse {
   count: number;
 }
 
-export interface CharacterState {
-  characters: Character[];
-  currentCharacter: Character | null;
-}
-
-export interface SetCharactersAction {
-  type: typeof SET_CHARACTERS;
-  payload: Character[];
-}
-
-export interface UpdateCharacterNameAction {
-  type: typeof UPDATE_CHARACTER_NAME;
-  payload: string;
-}
-
-export type CharacterAction = SetCharactersAction | UpdateCharacterNameAction;
-
-export const SET_CHARACTERS = 'SET_CHARACTERS';
-export const UPDATE_CHARACTER_NAME = 'UPDATE_CHARACTER_NAME';
-
 
   
