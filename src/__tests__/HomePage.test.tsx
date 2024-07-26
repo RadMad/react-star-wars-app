49c01eb320d0656e8ed5fc24e12432155b9698ac
Anton Radtsevich <ultimatovski@gmail.com>
2024-07-26 12:20:45 +0300

    unit tests fix

--- src/__tests__/HomePage.test.tsx
+++ src/__tests__/HomePage.test.tsx
@@ -18,7 +18,7 @@ vi.mock("@/api/starWarsApi", () => ({
 
 describe("HomePage", () => {
   const mockCharacters: CharactersResponse = {
-    count: 1,
+    count: 82,
     results: [
       {
         name: "Luke Skywalker",
@@ -31,6 +31,114 @@ describe("HomePage", () => {
         gender: "male",
         url: "https://swapi.dev/api/people/1/",
         id: 1,
+      },
+      {
+        name: "C-3PO",
+        height: "167",
+        mass: "75",
+        hair_color: "n/a",
+        skin_color: "gold",
+        eye_color: "yellow",
+        birth_year: "112BBY",
+        gender: "n/a",
+        url: "https://swapi.dev/api/people/2/",
+        id: 2,
+      },
+      {
+        name: "R2-D2",
+        height: "96",
+        mass: "32",
+        hair_color: "n/a",
+        skin_color: "white, blue",
+        eye_color: "red",
+        birth_year: "33BBY",
+        gender: "n/a",
+        url: "https://swapi.dev/api/people/3/",
+        id: 3,
+      },
+      {
+        name: "Darth Vader",
+        height: "202",
+        mass: "136",
+        hair_color: "none",
+        skin_color: "white",
+        eye_color: "yellow",
+        birth_year: "41.9BBY",
+        gender: "male",
+        url: "https://swapi.dev/api/people/4/",
+        id: 4,
+      },
+      {
+        name: "Leia Organa",
+        height: "150",
+        mass: "49",
+        hair_color: "brown",
+        skin_color: "light",
+        eye_color: "brown",
+        birth_year: "19BBY",
+        gender: "female",
+        url: "https://swapi.dev/api/people/5/",
+        id: 5,
+      },
+      {
+        name: "Owen Lars",
+        height: "178",
+        mass: "120",
+        hair_color: "brown, grey",
+        skin_color: "light",
+        eye_color: "blue",
+        birth_year: "52BBY",
+        gender: "male",
+        url: "https://swapi.dev/api/people/6/",
+        id: 6,
+      },
+      {
+        name: "Beru Whitesun lars",
+        height: "165",
+        mass: "75",
+        hair_color: "brown",
+        skin_color: "light",
+        eye_color: "blue",
+        birth_year: "47BBY",
+        gender: "female",
+        url: "https://swapi.dev/api/people/7/",
+        id: 7,
+      },
+      {
+        name: "R5-D4",
+        height: "97",
+        mass: "32",
+        hair_color: "n/a",
+        skin_color: "white, red",
+        eye_color: "red",
+        birth_year: "unknown",
+        gender: "n/a",
+        url: "https://swapi.dev/api/people/8/",
+        id: 8,
+      },
+      {
+        name: "Biggs Darklighter",
+        height: "183",
+        mass: "84",
+        hair_color: "black",
+        skin_color: "light",
+        eye_color: "brown",
+        birth_year: "24BBY",
+        gender: "male",
+        url: "https://swapi.dev/api/people/9/",
+        id: 9,
+      },
+      {
+        name: "Obi-Wan Kenobi",
+        height: "182",
+        mass: "77",
+        hair_color: "auburn, white",
+        skin_color: "fair",
+        eye_color: "blue-gray",
+        birth_year: "57BBY",
+        gender: "male",
+        url: "https://swapi.dev/api/people/10/",
+        id: 10,
       },
     ],
   };
@@ -116,9 +224,7 @@ describe("HomePage", () => {
   });
 
   test("displays error message if fetching data fails", async () => {
-    (fetchCharacters as Mock).mockRejectedValue(
-      new Error("Failed to fetch")
-    );
+    (fetchCharacters as Mock).mockRejectedValue(new Error("Failed to fetch"));
 
     await act(async () => {
       render(
@@ -152,7 +258,9 @@ describe("HomePage", () => {
 
     expect(fetchCharacters).toHaveBeenCalledWith(1, "");
 
-    const nextPageButton = screen.getByRole("button", { name: /next/i });
+    const nextPageButton = screen.getByRole("button", {
+      name: /next/i,
+    });
     fireEvent.click(nextPageButton);
 
     await waitFor(() => {
